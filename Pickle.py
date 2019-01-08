eaa1af4e2430f57a707e0193bc41bc0bc222bbfe
Govern <745087337@qq.com>
2019-01-08 16:24:29 +0800

    "Pickle"

--- Pickle.py
+++ Pickle.py
@@ -21,3 +21,30 @@ file = open('pickle_name.pickle', 'rb')
 b = pickle.load(file)
 file.close()
 print(b)
+
+"""
+------------------------------------------------------------------------------
+"""
+
+class Data():
+    def __init__(self):
+        self.Data1 = 23
+        self.Data2 = {
+            "mother":"pump",
+            "sex":"male",
+            "age":23
+        }
+#pickle a variable to a file
+data = Data()
+file =  open('pickle_name.pickle','wb')
+pickle.dump(data, file)
+file.close()
+
+"""
+Reload
+"""
+file = open('pickle_name.pickle', 'rb')
+b = pickle.load(file)
+file.close()
+print(b.Data1)
+print(b.Data2)
